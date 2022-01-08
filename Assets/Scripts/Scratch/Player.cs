dafaa03df6e65d077de77e3b83f8512b67ecf811
Sami Batuhan Basmaz Ölmez <repelliuss@gmail.com>
2022-01-08 22:09:42 +0300

    add first person and free camera

---
+++ Assets/Scripts/Scratch/Player.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player : MonoBehaviour {
+    FreeFlyCamera free;
+    RigidbodyFirstPersonController fps;
+    Rigidbody rigid;
+
+    void Start()
+    {
+        free = GetComponent<FreeFlyCamera>();
+        fps = GetComponent<RigidbodyFirstPersonController>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public string GetCameraName()
+    {
+        if(fps.enabled) return "FIRST PERSON";
+        else return "FREE CAM";
+    }
+
+    public void SwitchCamera()
+    {
+        if(fps.enabled)
+        {
+            fps.enabled = false;
+            free.enabled = true;
+            rigid.useGravity = false;
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+        else
+        {
+            fps.enabled = true;
+            free.enabled = false;
+            rigid.useGravity = true;
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+    }
+}
